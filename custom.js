c15da709028b2133acbe3a639a289ab385d68319
eslam  mohamed <99561435+eslam-mohamedl@users.noreply.github.com>
2022-07-05 23:55:02 +0200

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,21 @@
+
+
+let dark = document.getElementById("dark"),
+     btn = document.getElementById("btn-dark"),
+     heading = document.querySelectorAll("body h2"),
+     nav = document.querySelectorAll("header .header-contant .nav ul li .navgaition");
+
+
+     
+
+
+     btn.onclick = function(){
+
+        dark.classList.toggle("dark");
+
+
+     }
+
+
+
+
